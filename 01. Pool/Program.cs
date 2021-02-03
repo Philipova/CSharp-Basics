daf32431801635dfb26c7467e61eb1815d77183f
Philipova <75500722+Philipova@users.noreply.github.com>
2021-02-03 23:46:44 +0200

    Add files via upload

---
+++ 01. Pool/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01._Pool_ex_exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double people = double.Parse(Console.ReadLine());
+            double entranceTax = double.Parse(Console.ReadLine());
+            double loungePrice = double.Parse(Console.ReadLine());
+            double umbrellaPrice = double.Parse(Console.ReadLine());
+
+            double totalEntrancePrice = people * entranceTax;
+            double umbrella = Math.Ceiling(people / 2);
+            double lounge = Math.Ceiling(people * 0.75);
+
+            double totalPrice = totalEntrancePrice + lounge * loungePrice + umbrella * umbrellaPrice;
+
+            Console.WriteLine($"{totalPrice:F2} lv.");
+        }
+    }
+}
