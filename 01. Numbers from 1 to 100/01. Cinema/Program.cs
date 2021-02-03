8ed75bd69ada9a9eac66ad99de6a0de83641c9af
Philipova <75500722+Philipova@users.noreply.github.com>
2021-02-03 16:56:14 +0200

    Add files via upload

---
+++ 01. Numbers from 1 to 100/01. Cinema/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01._Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int columns = int.Parse(Console.ReadLine());
+            double incomes = 0;
+
+            if (type == "Premiere")
+            {
+                incomes = rows * columns * 12.00;
+            }
+            else if (type == "Normal")
+            {
+                incomes = rows * columns * 7.50;
+            }
+            else if (type == "Discount")
+            {
+                incomes = rows * columns * 5.00;
+            }
+
+            //Console.WriteLine("{ 0:f2} leva", incomes);
+            Console.WriteLine($"{incomes:F2} leva");
+        }
+    }
+}
