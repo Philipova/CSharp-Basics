6363049b3f8b5ba693f5f6d9c01a758fd1cc043a
Philipova <75500722+Philipova@users.noreply.github.com>
2021-02-06 21:28:51 +0200

    Add files via upload

---
+++ 01. Number Pyramid/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01._Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int current = 1;
+            bool isBigger = false;
+
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int cols = 0; cols <= rows; cols++)
+                {
+                    if (current > 1)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    Console.WriteLine(current + " ");
+                    current++;
+                }
+
+            if (isBigger)
+            {
+            break;
+
+            }
+                Console.WriteLine();
+            }
+        }
+    }
+}
