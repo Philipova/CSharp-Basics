129b57378c5a6c6de9a202ca13cff5e4557ae45c
Philipova <75500722+Philipova@users.noreply.github.com>
2021-02-17 18:41:06 +0200

    Add files via upload

---
+++ 02. Summer Outfit/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace _02._Summer_Outfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double degrees = double.Parse(Console.ReadLine());
+            string timeOfTheDay = Console.ReadLine();
+            string outfit = "";
+            string shoes = "";
+
+            if (degrees >= 10 && degrees <= 18)
+            {
+                if (timeOfTheDay == "Morning")
+                {
+                    outfit = "Sweatshirt";
+                    shoes = "Sneakers";
+                }
+
+                else if (timeOfTheDay == "Afternoon")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+
+                else if (timeOfTheDay == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+
+            }
+
+            if (degrees > 18 && degrees <= 24)
+            {
+                if (timeOfTheDay == "Morning")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+
+                else if (timeOfTheDay == "Afternoon")
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+
+                else if (timeOfTheDay == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+            }
+            if (degrees >= 25)
+            {
+                if (timeOfTheDay == "Morning")
+                {
+                    outfit = "T-Shirt";
+                    shoes = "Sandals";
+                }
+
+                else if (timeOfTheDay == "Afternoon")
+                {
+                    outfit = "Swim Suit";
+                    shoes = "Barefoot";
+                }
+
+                else if (timeOfTheDay == "Evening")
+                {
+                    outfit = "Shirt";
+                    shoes = "Moccasins";
+                }
+
+            }
+
+            Console.WriteLine($"It's {degrees} degrees, get your {outfit} and {shoes}.");
+
+        }
+    }
+}
