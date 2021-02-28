062ee60372a477d0d3036380d6db7c851ffa6783
Philipova <75500722+Philipova@users.noreply.github.com>
2021-02-28 22:43:36 +0200

    Add files via upload

---
+++ 04. Sum of Two Numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _04._Sum_of_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+            bool flag = false;
+
+            for (int num1 = start; num1 <= end; num1++)
+            {
+                for (int num2 = start; num2 <= end; num2++)
+                {
+                    counter++;
+                    if (num1 + num2 == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{counter} ({num1} + {num2} = {magicNumber})");
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag)
+                {
+                    break;
+                }
+            }
+            if (!flag)
+            {
+                Console.WriteLine($"{counter} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
