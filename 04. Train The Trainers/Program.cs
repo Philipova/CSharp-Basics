756e13ec01f6947efa89b6e435e425d6327d4bd5
Philipova <75500722+Philipova@users.noreply.github.com>
2021-03-01 11:57:53 +0200

    Add files via upload

---
+++ 04. Train The Trainers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _04._Train_The_Trainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // read jury count
+            int juryCount = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            double marksSum = 0;
+            int presentationCount = 0;
+            // while ! Finish -> read presentation name
+            while (input != "Finish")
+            {
+                presentationCount++; 
+                double markSum = 0;
+                
+                for (int i = 1; i <= juryCount; i++)
+                {
+                    double mark = double.Parse(Console.ReadLine());
+                    markSum += mark;
+                }
+                double avgMark = markSum / juryCount;
+                marksSum += avgMark;
+                Console.WriteLine($"{input} - {avgMark:F2}.");
+                input = Console.ReadLine();
+            }
+            // for 1 to jurycount
+            // read mark
+            // calculate avg mark from presentation
+            // print avg mark
+            // print total result
+            double finalMark = marksSum / presentationCount;
+            Console.WriteLine($"Student's final assessment is {finalMark:F2}.");
+        }
+    }
+}
