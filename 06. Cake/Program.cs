453e8c52dda301d420d523827ab6babffbe1fe19
Philipova <75500722+Philipova@users.noreply.github.com>
2021-03-11 10:06:14 +0200

    Add files via upload

---
+++ 06. Cake/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+
+            int totalCakeSlices = width * height;
+            int finalCakeSlices = width * height;
+            int totalTakenSlices = 0;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "STOP")
+                {
+                    break;
+                }
+
+                int takenSlices = int.Parse(input);
+
+                totalCakeSlices -= takenSlices;
+                totalTakenSlices += takenSlices;
+
+                if (totalCakeSlices <= 0)
+                {
+                    //int finalCakeSlices = width * height;
+                    Console.WriteLine($"No more cake left! You need {totalTakenSlices - finalCakeSlices} pieces more.");
+                    break;
+                }
+            }
+
+            if (totalCakeSlices > 0)
+            {
+                Console.WriteLine($"{totalTakenSlices - totalTakenSlices} pieces are left.");
+            }
+        }
+    }
+}
