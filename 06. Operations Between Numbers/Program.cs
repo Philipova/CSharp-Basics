90b326b792e23b2328bab743a4051dca1924cde8
Philipova <75500722+Philipova@users.noreply.github.com>
2021-03-13 10:01:07 +0200

    Add files via upload

---
+++ 06. Operations Between Numbers/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace _06._Operations_Between_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            char operation = char.Parse(Console.ReadLine());
+
+            //„+“, „-“, „*“, „/“, „%“ 1. резултата; 2. проверка за резултата - четно или нечетно число;
+
+            switch (operation)
+            {
+                case '+':
+                    int sum = number1 + number2;
+                    if (sum % 2 == 0)
+                    {
+                        Console.WriteLine($"{number1} + {number2} = {sum} - even");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{number1} + {number2} = {sum} - odd");
+                    }
+                    break;
+                case '-':
+                    int diff = number1 - number2;
+                    if (diff % 2 == 0)
+                    {
+                        Console.WriteLine($"{number1} - {number2} = {diff} - even");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{number1} - {number2} = {diff} - odd");
+                    }
+                    break;
+                case '*':
+                    int result = number1 * number2;
+                    if (result % 2 == 0)
+                    {
+                        Console.WriteLine($"{number1} * {number2} = {result} - even");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{number1} * {number2} = {result} - odd");
+                    }
+                    break;
+                case '/':
+                    if (number2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {number1} by zero");
+
+                    }
+                    else
+                    {
+                        double division = (number1*1.0)/number2;
+                        Console.WriteLine($"{number1} / {number2} = {division:F2}");
+                    }
+                    break;
+                case '%':
+                    if (number2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {number1} by zero");
+
+                    }
+                    else
+                    {
+                        double leftover = number1 % number2;
+                        Console.WriteLine($"{number1} % {number2} = {leftover}");
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
