0585dd88c68b6a050e4ab10f560d7a4b3abe5621
Philipova <75500722+Philipova@users.noreply.github.com>
2021-03-24 14:15:39 +0200

    Add files via upload

---
+++ 10. Odd Even Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _10._Odd_Even_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int element = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    evenSum += element;
+                }
+                else
+                {
+                    oddSum += element;
+                }
+
+            }
+            if (evenSum == oddSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + oddSum);
+            }
+            else
+            {
+                int difference = evenSum - oddSum;
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + Math.Abs(difference));
+            }
+        }
+    }
+}
