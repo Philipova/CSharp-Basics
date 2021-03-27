4ab1e16cf48f71d4bcea8110b3d6398663523e22
Philipova <75500722+Philipova@users.noreply.github.com>
2021-03-27 09:11:38 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int maxNum = int.MinValue;
+            int minNum = int.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int current = int.Parse(Console.ReadLine());
+
+                if (current > maxNum)
+                {
+                    maxNum = current;
+                }
+                if (current < minNum)
+                {
+                    minNum = current;
+                }
+            }
+            //Max number: 304
+            //Min number: 0
+            Console.WriteLine($"Max number: {maxNum}");
+            Console.WriteLine($"Min number: {minNum}");
+
+        }
+    }
+}
