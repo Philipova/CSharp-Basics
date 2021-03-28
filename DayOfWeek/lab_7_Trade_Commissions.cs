004f8e9a77bb79d0746592f7a0218646709a49e8
Philipova <75500722+Philipova@users.noreply.github.com>
2021-03-28 16:16:19 +0300

    Add files via upload

---
+++ DayOfWeek/lab_7_Trade_Commissions.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+            double commission = 0;
+            bool isCityAndSales = false;
+
+            switch (city)
+            {
+                case "Sofia":
+                    if (0 <= sales && sales <= 500)
+                    {
+                        commission = 0.05;
+                        isCityAndSales = true;
+                    }
+                    else if (500 < sales && sales <= 1000)
+                    {
+                        commission = 0.07;
+                        isCityAndSales = true;
+                    }
+                    else if (1000 < sales && sales <= 10000)
+                    {
+                        commission = 0.08;
+                        isCityAndSales = true;
+                    }
+                    else if (sales < 10000)
+                    {
+                        commission = 0.12;
+                        isCityAndSales = true;
+                    }
+                    break;
+
+                case "Varna":
+                    if (0 <= sales && sales <= 500)
+                    {
+                        commission = 0.045;
+                        isCityAndSales = true;
+                    }
+                    else if (500 < sales && sales <= 1000)
+                    {
+                        commission = 0.075;
+                        isCityAndSales = true;
+                    }
+                    else if (1000 < sales && sales <= 10000)
+                    {
+                        commission = 0.1;
+                        isCityAndSales = true;
+                    }
+                    else if (sales < 10000)
+                    {
+                        commission = 0.13;
+                        isCityAndSales = true;
+                    }
+                    break;
+
+                case "Plovdiv":
+                    if (0 <= sales && sales <= 500)
+                    {
+                        commission = 0.055;
+                        isCityAndSales = true;
+                    }
+                    else if (500 < sales && sales <= 1000)
+                    {
+                        commission = 0.08;
+                        isCityAndSales = true;
+                    }
+                    else if (1000 < sales && sales <= 10000)
+                    {
+                        commission = 0.12;
+                        isCityAndSales = true;
+                    }
+                    else if (sales < 10000)
+                    {
+                        commission = 0.145;
+                        isCityAndSales = true;
+                    }
+                    break;
+            }
+            if (isCityAndSales == true)
+            {
+                double result = sales * commission;
+                Console.WriteLine($"{result:F2}");
+            }
+            else if (isCityAndSales == false)
+            {
+                Console.WriteLine("error");
+            }
+        }
+    }
+}
